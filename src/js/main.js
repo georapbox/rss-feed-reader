368a1092e5348262ee089a6e2b54634750d3a196
georapbox <georapbox@gmail.com>
2022-06-15 12:54:13 +0300

    Remove site footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import 'construct-style-sheets-polyfill/dist/adoptedStyleSheets.js';
 import { styleSheets } from './helpers/styles.js';
 import './components/site-header.js';
-import './components/site-footer.js';
 import './components/add-feed.js';
 import './components/feeds-list.js';
 import './components/feed-reader.js';
