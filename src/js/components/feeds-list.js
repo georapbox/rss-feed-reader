cc441168eec67a1ed5f203468ed4a57ced16afc8
georapbox <georapbox@gmail.com>
2022-07-07 20:43:45 +0300

    Fix z-index of reload alert

--- src/js/components/feeds-list.js
+++ src/js/components/feeds-list.js
@@ -26,7 +26,7 @@ template.innerHTML = /* html */`
     <export-feeds class="mt-2"></export-feeds>
   </details>
 
-  <div class="alert alert-info py-2 text-center rounded-0 position-fixed top-0 start-0 w-100 d-none" role="alert" id="reloadInfo">
+  <div class="alert alert-info py-2 text-center rounded-0 position-fixed top-0 start-0 w-100 m-0 d-none" style="z-index: 1;" role="alert" id="reloadInfo">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="min-width: 16px;" fill="currentColor" class="bi bi-info-circle-fill me-2" viewBox="0 0 16 16">
       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
     </svg>
