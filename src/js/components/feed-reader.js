fd78624e1cbbca0b219fdfababf3a08e1c2a1f3d
georapbox <georapbox@gmail.com>
2022-10-13 10:07:22 +0300

    Cancel fetch request on dialog close

--- src/js/components/feed-reader.js
+++ src/js/components/feed-reader.js
@@ -4,6 +4,8 @@ import { fetchFeed } from '../helpers/fetch-feeds.js';
 
 const template = document.createElement('template');
 
+let controller;
+
 template.innerHTML = /* html */`
   <style>
     :host {
@@ -34,12 +36,14 @@ template.innerHTML = /* html */`
       <div class="row">
         <div class="col-xl-10 offset-xl-1">
           <div class="modal-header border-0 px-0 justify-content-start">
-            <button type="button" id="closeDialogBtn" class="btn btn-default text-primary me-2">
-              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-              </svg>
-              <span class="visually-hidden">Back</span>
-            </button>
+            <form method="dialog">
+              <button type="submit" class="btn btn-default text-primary me-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                </svg>
+                <span class="visually-hidden">Back</span>
+              </button>
+            </form>
 
             <h2 class="modal-title h4 text-truncate"></h2>
           </div>
@@ -69,54 +73,34 @@ class FeedReader extends HTMLElement {
       this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
-    this._loading = false;
-
     this.spinnerEl = this.shadowRoot.getElementById('spinner');
-    this.dialog = this.shadowRoot.querySelector('dialog');
-    this.modalTitle = this.dialog.querySelector('.modal-title');
+    this.dialogEl = this.shadowRoot.querySelector('dialog');
+    this.modalTitle = this.dialogEl.querySelector('.modal-title');
     this.feedsViewer = this.shadowRoot.getElementById('feedsViewer');
     this.errorEl = this.shadowRoot.getElementById('error');
-    this.closeDialogBtn = this.shadowRoot.getElementById('closeDialogBtn');
 
     this.shadowRoot.adoptedStyleSheets = styleSheets;
   }
 
   connectedCallback() {
     document.addEventListener('display-feed', this._onDialogOpen);
-    this.dialog.addEventListener('cancel', this._onDialogCancel);
-    this.dialog.addEventListener('close', this._onDialogClose);
-    this.closeDialogBtn.addEventListener('click', this._closeDialog);
+    this.dialogEl.addEventListener('close', this._onDialogClose);
   }
 
   disconnectedCallback() {
     document.removeEventListener('display-feed', this._onDialogOpen);
-    this.dialog.removeEventListener('cancel', this._onDialogCancel);
-    this.dialog.removeEventListener('close', this._onDialogClose);
-    this.closeDialogBtn.removeEventListener('click', this._closeDialog);
+    this.dialogEl.removeEventListener('close', this._onDialogClose);
   }
 
-  _closeDialog = () => {
-    if (this._loading) {
-      return;
-    }
-
-    this.dialog.close();
-  };
-
-  _onDialogCancel = evt => {
-    if (this._loading) {
-      evt.preventDefault();
-    }
-  };
-
   _onDialogClose = () => {
+    controller && controller.abort();
     document.body.classList.remove('overflow-hidden');
     this._resetDialogContent();
   };
 
   _onDialogOpen = evt => {
     document.body.classList.add('overflow-hidden');
-    this.dialog.showModal();
+    this.dialogEl.showModal();
     this._renderFeed(evt.detail.feedUrl);
   };
 
@@ -132,13 +116,14 @@ class FeedReader extends HTMLElement {
     const feed = feeds.find(item => item.url === feedUrl);
 
     if (feed) {
-      this._loading = true;
-
       this.spinnerEl.classList.remove('d-none');
-      this.closeDialogBtn.classList.add('d-none');
+
+      controller = new AbortController();
 
       try {
-        const data = await fetchFeed(feed.url);
+        const data = await fetchFeed(feed.url, {
+          signal: controller.signal
+        });
 
         this.modalTitle.textContent = data.feed.title || feed.url;
 
@@ -146,13 +131,13 @@ class FeedReader extends HTMLElement {
           this.feedsViewer.insertAdjacentHTML('beforeend', this._feedsReaderTemplate(item));
         });
       } catch (error) {
-        console.error(error);
-        this.modalTitle.textContent = '';
-        this.errorEl.classList.remove('d-none');
+        if (error.name !== 'AbortError') {
+          console.error(error);
+          this.modalTitle.textContent = '';
+          this.errorEl.classList.remove('d-none');
+        }
       } finally {
         this.spinnerEl.classList.add('d-none');
-        this.closeDialogBtn.classList.remove('d-none');
-        this._loading = false;
       }
     }
   }
