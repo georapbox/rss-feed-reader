3f5970ac16bc1c509bfb8ae3c29127959a704e12
georapbox <georapbox@gmail.com>
2023-09-22 18:48:04 +0300

    Remove side-header component

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ import '@georapbox/clipboard-copy-element/dist/clipboard-copy-defined.js';
 import '@georapbox/web-share-element/dist/web-share-defined.js';
 import 'construct-style-sheets-polyfill/dist/adoptedStyleSheets.js';
 import { styleSheets } from './helpers/styles.js';
-import './components/site-header.js';
 import './components/add-feed.js';
 import './components/feeds-list.js';
 import './components/feed-reader.js';
